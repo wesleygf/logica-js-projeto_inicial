e7c762b8ba35a171ccfb60631a567c33e7e2d66d
Wesley Mac <Wesleygfsilva@hotmail.com>
2024-10-25 13:22:46 +1100

    comecando logica do app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+alert("Boas vindas ao jogo do numero secreto");
+let numeroSecreto =29;
+let chute = prompt('Escolha um numero entre 1 e 30');
+
+if(numeroSecreto == chute){
+    console.log('Isso ai voce acertou o numero secreto');
+}
+    
